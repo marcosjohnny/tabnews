842ffa4cf5474d1980c5bed226eb1063c22db2fc
Marcos Rodrigues <38053209+marcosjohnny@users.noreply.github.com>
2023-06-23 23:11:01 +0000

    added scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews
+
 Implementação do https://www.tabnews.com.br
--- pages/api/v1/dados.js
+++ pages/api/v1/dados.js
@@ -1,5 +1,5 @@
 function Dados(req, res) {
-    return res.status(200).json({"teste": "123"})
+  return res.status(200).json({ teste: "123" });
 }
 
 export default Dados;
