21cf2e65df98263f2d94e861cfe1633ef4f390e3
Marcos Rodrigues <38053209+marcosjohnny@users.noreply.github.com>
2023-10-31 00:41:09 +0000

    :sparkles: feat: added `database.js` and `.env` files

--- infra/database.js
+++ infra/database.js
@@ -5,7 +5,7 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
